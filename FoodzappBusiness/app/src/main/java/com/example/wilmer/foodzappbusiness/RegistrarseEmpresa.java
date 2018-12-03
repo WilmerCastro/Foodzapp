244389c031bf29c0d5ad931678d96e60062cd71b
WilmerCastro <wilmer19427@gmail.com>
2018-12-03 13:49:42 -0400

    Para empresas

---
+++ FoodzappBusiness/app/src/main/java/com/example/wilmer/foodzappbusiness/RegistrarseEmpresa.java
@@ -0,0 +1,13 @@
+package com.example.wilmer.foodzappbusiness;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RegistrarseEmpresa extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registrarse_empresa);
+    }
+}
